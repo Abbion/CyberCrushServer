790c62b316b85af3f30da306e7a8418b35ac5ec1
Amadeusz <amadeuszwalag@gmail.com>
2025-09-11 16:59:34 +0200

    authentication server initial commit

---
+++ cyber_crush_authentication_server/src/main.rs
@@ -0,0 +1,49 @@
+use axum::{
+    extract::Json,
+    response::IntoResponse,
+    routing::{get, post},
+    Router,
+};
+
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+#[derive(Debug, Deserialize)]
+struct LoginRequest {
+    username: String,
+    password: String,
+}
+
+#[derive(Debug, Serialize)]
+struct LoginResponse {
+    username: String,
+    password: String,
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(hello_world))
+        .route("/login", post(login));
+
+    let addr = SocketAddr::from(([0,0,0,0], 3000));
+    println!("Server running at: {}", addr);
+
+    let listener = TcpListener::bind(addr).await.unwrap();
+    
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_world() -> &'static str {
+    "Hello, cyber crush!"
+}
+
+async fn login(Json(payload): Json<LoginRequest>) -> impl IntoResponse {
+    let response = LoginResponse {
+        username: format!("{}@", payload.username),
+        password: format!("{}@", payload.password),
+    };
+
+    Json(response)
+}
