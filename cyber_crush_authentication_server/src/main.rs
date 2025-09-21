c943b3220c2bce5b33d05e77a9693189bbb852a1
Amadeusz <amadeuszwalag@gmail.com>
2025-09-21 11:17:48 +0200

    Token generation

--- cyber_crush_authentication_server/src/main.rs
+++ cyber_crush_authentication_server/src/main.rs
@@ -10,6 +10,8 @@ use std::{fs, net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use argon2::{Argon2, PasswordHash, password_hash, PasswordVerifier};
+use rand::{TryRngCore, rngs::OsRng};
+use hex;
 
 #[derive(Debug, Deserialize)]
 struct LoginRequest {
@@ -81,23 +83,36 @@ async fn hello_world() -> &'static str {
 }
 
 async fn login(State(state): State<Arc<ServerState>>, Json(payload): Json<LoginRequest>) -> impl IntoResponse {
-    let password_query = sqlx::query_scalar::<_, String>(
-        r#"SELECT password FROM users WHERE username = $1"#
+    #[derive(sqlx::FromRow)]
+    struct PasswordQuery {
+        id: i32,
+        password: String
+    }
+
+    let password_query = sqlx::query_as::<_, PasswordQuery>(
+        r#"SELECT id, password FROM users WHERE username = $1"#
         )
         .bind(&payload.username)
         .fetch_optional(&state.db_pool)
         .await;
 
     let response = match password_query {
-        Ok(Some(stored_password_hash)) => {
-            let verified = verify_password(&stored_password_hash, &payload.password, &state.pepper);
+        Ok(Some(password_query)) => {
+            let verified = verify_password(&password_query.password, &payload.password, &state.pepper);
 
             if verified.is_err() {
                 eprintln!("Error: Login failed for user: {}. Error: {}", payload.username, verified.unwrap_err());
                 LoginResponse{ success: false, message: "Password decodeing error.".into(), token: "".into() }
             }
             else if verified.unwrap() == true {
-                LoginResponse{ success: true, message: "success".into(), token: "test Token".into() }
+                match generate_and_store_token(&state.db_pool, password_query.id).await {
+                    Ok(token) => {
+                        LoginResponse{ success: true, message: "success".into(), token: token }
+                    },
+                    Err(error) => {
+                        LoginResponse{ success: false, message: error, token: "".into() }
+                    }
+                }
             }
             else {
                 LoginResponse{ success: false, message: "Wrong credentials.".into(), token: "".into() }
@@ -108,7 +123,7 @@ async fn login(State(state): State<Arc<ServerState>>, Json(payload): Json<LoginR
         },
         Err(error) => {
             eprintln!("Error: Login failed for user: {}. Error: {}", payload.username, error);
-            LoginResponse{ success: false, message: "Login database error!".into(), token: "".into() }
+            LoginResponse{ success: false, message: "Login database error.".into(), token: "".into() }
         }
     };
 
@@ -139,4 +154,59 @@ fn verify_password(stored_hash: &str, password: &str, pepper: &str) -> Result<bo
     let parsed_hash = PasswordHash::new(stored_hash)?;
 
     Ok(argon2.verify_password(pepper_pass.as_bytes(), &parsed_hash).is_ok())
+}
+
+fn generate_token() -> String {
+    let mut buffer = [0u8; 32];
+    OsRng.try_fill_bytes(&mut buffer).unwrap();
+    hex::encode(buffer)
+}
+
+async fn generate_and_store_token(db_pool: &PgPool, user_id: i32) -> Result<String, String> {
+    const MAX_ATTEMPTS: usize = 8;
+
+    for attempt in 0..MAX_ATTEMPTS {
+        let token = generate_token();
+
+        let token_collision = sqlx::query_scalar::<_, i64>("SELECT 1 FROM users WHERE user_token = $1")
+            .bind(&token)
+            .fetch_optional(db_pool)
+            .await;
+
+        let token_collision = match token_collision {
+            Ok(state) => state,
+            Err(error) => {
+                eprintln!("Error: Failed to check token collision for user_id {}: {}", user_id, error);
+                return Err("Database error. Token collision.".into());
+            }
+        };
+
+        if token_collision.is_some() {
+            eprintln!("Info: token collision has occured! Retrying attempt {}/{}", attempt + 1, MAX_ATTEMPTS);
+            continue;
+        }
+
+        let token_update = sqlx::query("UPDATE users SET user_token = $1 WHERE id = $2")
+            .bind(&token)
+            .bind(&user_id)
+            .execute(db_pool)
+            .await;
+
+        let update_result = match token_update {
+            Ok(result) => result,
+            Err(error) => {
+                eprintln!("Error: Failed to update the user_id {} token: {}", user_id, error);
+                return Err("Database error. Token update error.".into())
+            }
+        };
+
+        if update_result.rows_affected() == 1 {
+            return Ok(token);
+        }
+        
+        eprintln!("Info: Generate token request was completed for user_id {}, but no user was found.", user_id);
+        return Err("Database error. No user for token.".into());
+    }
+
+    Err("No token yielded.".into())
 }
